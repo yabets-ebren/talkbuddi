2f222301ecb4f10a2f674db526876690115f00da
Yabets Ebren <unosjabez@gmail.com>
2022-06-27 15:16:57 -0600

    features added6

--- components/Board.jsx
+++ components/Board.jsx
@@ -8,7 +8,7 @@ const Board = ({image, bgColor, fontColor,borderColor, title, backTitle, backdes
   return (
       <div onClick={()=>{setFlip('rotate-y-180')}} className={`w-90 mx-auto h-96 relative cursor-pointer ${flip} transition-all ease-in-out duration-1000  transfrom-style3d`}>
           <div className={` flex flex-col z-20 items-center justify-between ${bgColor}  absolute delay-1000 pt-10 md:pt-12  h-full w-full  backface-hidden`}>
-            <div className='text-white font-montserat text-center px-5 text-xl md:px-0'>
+            <div className='text-white bg-purple font-montserat text-center px-5 text-xl md:px-0'>
               {title}
             </div>
             <div>
