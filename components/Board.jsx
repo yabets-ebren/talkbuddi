00524b2bb088b45626ad6c7063f2727692072a89
Yabets Ebren <unosjabez@gmail.com>
2022-06-27 14:24:45 -0600

    features added4

--- components/Board.jsx
+++ components/Board.jsx
@@ -3,17 +3,15 @@ import React, { useState } from 'react'
 
 const Board = ({image, bgColor, fontColor,borderColor, title, backTitle, backdesc}) => {
 
-  // const arr = backdesc.split(',')
-  console.log(""+typeof(backdesc))
   const [flip, setFlip] = useState('');
   return (
       <div onClick={()=>{setFlip('rotate-y-180')}} className={`w-90 mx-auto h-96 relative cursor-pointer ${flip} transition-all ease-in-out duration-1000  transfrom-style3d`}>
-          <div className={` flex flex-col z-20 items-center justify-between ${bgColor}  absolute delay-1000  h-full w-full pt-10 md:pt-12 backface-hidden`}>
+          <div className={` flex flex-col z-20 items-center justify-between ${bgColor}  absolute delay-1000 pt-10 md:pt-12  h-full w-full  backface-hidden`}>
             <div className='text-white font-montserat px-4 text-center  text-xl'>
               {title}
             </div>
             <div>
-              <div className='scale-75 w-80 h-72 md:scale-100'>
+              <div className=' w-64 h-60  relative md:w-80 md:h-72'>
                   {image}
               </div>
             </div>
